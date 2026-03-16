a3d96dbec782417e2eaa9a84ec5d40c2879dd479
Sorann753 <Arthusd@hotmail.fr>
2026-03-16 02:17:12 +0100

    updating FLTK package
    - added the version 1.3.10 and 1.4.4 of the FLTK package
    - added an actual on_install for glu in order to provide the dependency needed by FLTK in wayland mode

--- packages/f/fltk/xmake.lua
+++ packages/f/fltk/xmake.lua
@@ -8,6 +8,8 @@ package("fltk")
 
     add_versions("home:1.3.9", "103441134915402808fd45424d4061778609437e804334434e946cfd26b196c2")
     add_versions("github:1.3.9", "f30661851a61f1931eaaceb9ef4005584c85cb07fd7ffc38a645172b8e4eb3df")
+    add_versions("github:1.3.10", "0cbe376eaba35c17ba881d38c1b5964ca9f8055726b0a889c6425125081bc654")
+    add_versions("github:1.4.4", "cbf5f7846af596206e8e4489e14c9981f98d7b37168110a00dcd26d8d479a669")
 
     add_patches("1.3.9", "patches/1.3.9/cmake-fluid.patch", "06ee1e82a74651a0b4ba4b386e5e5436d8b95584330d02a8a2c53351210a9127")
 
@@ -20,14 +22,15 @@ package("fltk")
 
     if is_plat("windows", "mingw") then
         add_syslinks("ws2_32", "comctl32", "gdi32", "oleaut32", "ole32", "uuid", "shell32", "advapi32", "comdlg32", "winspool", "user32", "kernel32", "odbc32")
-    elseif is_plat("macosx") then 
+    elseif is_plat("macosx") then
         add_frameworks("Cocoa")
     elseif is_plat("android") then
         add_syslinks("android")
         add_syslinks("dl")
     elseif is_plat("linux") then
         add_syslinks("dl", "pthread")
-        add_deps("libx11", "libxext", "libxinerama", "libxcursor", "libxrender", "libxfixes", "fontconfig") 
+        add_deps("libx11", "libxext", "libxinerama", "libxcursor", "libxrender", "libxfixes", "fontconfig")
+        add_deps("glu", "wayland", "pkgconf")
     end
 
     add_deps("cmake")
@@ -38,10 +41,10 @@ package("fltk")
             if package:version() and package:version():eq("1.3.9") then
                 assert(not package:config("fluid"), "package(fltk/1.3.9): Unsupported fluid on linux")
             end
-            if package:config("pango") then 
+            if package:config("pango") then
                 package:add("deps", "pango-1.0", "pangoxft-1.0", "gobject-2.0", "cairo", "pangocairo-1.0")
             end
-            if package:config("xft") then 
+            if package:config("xft") then
                 package:add("deps", "libxft")
             end
         end
@@ -52,23 +55,43 @@ package("fltk")
             io.replace(file, "<libpng/png.h>", "<png.h>", {plain = true})
         end
 
+        local version_over_14X = package:version() and package:version():ge("1.4")
+
         local configs = {
             "-DFLTK_BUILD_TEST=OFF",
             "-DFLTK_BUILD_EXAMPLES=OFF",
-            "-DOPTION_USE_SYSTEM_LIBPNG=ON",
-            "-DOPTION_USE_SYSTEM_ZLIB=ON",
-            "-DOPTION_USE_SYSTEM_LIBJPEG=ON"
         }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
-        table.insert(configs, "-DOPTION_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DFLTK_MSVC_RUNTIME_DLL=" .. (package:has_runtime("MD") and "ON" or "OFF"))
         table.insert(configs, "-DFLTK_BUILD_FLUID=" .. (package:config("fluid") and "ON" or "OFF"))
         table.insert(configs, "-DFLTK_BUILD_FORMS=" .. (package:config("forms") and "ON" or "OFF"))
-        if package:is_plat("linux") then
-            table.insert(configs, "-DOPTION_USE_PANGO=" .. (package:config("pango") and "ON" or "OFF"))
-            table.insert(configs, "-DOPTION_USE_XFT=" .. (package:config("xft") and "ON" or "OFF"))
+
+        -- they changed the name of the config flags in 1.4.0
+        if version_over_14X then
+            table.insert(configs, "-DFLTK_USE_SYSTEM_LIBPNG=ON")
+            table.insert(configs, "-DFLTK_USE_SYSTEM_ZLIB=ON")
+            table.insert(configs, "-DFLTK_USE_SYSTEM_LIBJPEG=ON")
+            table.insert(configs, "-DFLTK_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        else
+            table.insert(configs, "-DOPTION_USE_SYSTEM_LIBPNG=ON")
+            table.insert(configs, "-DOPTION_USE_SYSTEM_ZLIB=ON")
+            table.insert(configs, "-DOPTION_USE_SYSTEM_LIBJPEG=ON")
+            table.insert(configs, "-DOPTION_BUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         end
-        import("package.tools.cmake").install(package, configs)
+
+        local opt = {}
+        if package:is_plat("linux") then
+            if version_over_14X then
+                table.insert(configs, "-DFLTK_USE_PANGO=" .. (package:config("pango") and "ON" or "OFF"))
+                table.insert(configs, "-DFLTK_USE_XFT=" .. (package:config("xft") and "ON" or "OFF"))
+            else
+                table.insert(configs, "-DOPTION_USE_PANGO=" .. (package:config("pango") and "ON" or "OFF"))
+                table.insert(configs, "-DOPTION_USE_XFT=" .. (package:config("xft") and "ON" or "OFF"))
+            end
+
+            opt.envs = import("package.tools.cmake").buildenvs(package)
+        end
+        import("package.tools.cmake").install(package, configs, opt)
     end)
 
     on_test(function (package)
--- packages/g/glu/xmake.lua
+++ packages/g/glu/xmake.lua
@@ -3,6 +3,14 @@ package("glu")
     set_homepage("https://gitlab.freedesktop.org/mesa/glu")
     set_description("OpenGL utility library")
 
+    add_urls("https://archive.mesa3d.org/glu/glu-$(version).tar.xz")
+    add_versions("9.0.3", "bd43fe12f374b1192eb15fe20e45ff456b9bc26ab57f0eee919f96ca0f8a330f")
+
+    if is_plat("linux") then
+        add_deps("meson", "ninja")
+        add_syslinks("GL")
+    end
+
     on_fetch(function (package, opt)
         if package:is_plat("macosx") then
             return {frameworks = "OpenGL", defines = "GL_SILENCE_DEPRECATION"}
@@ -14,4 +22,14 @@ package("glu")
                 return package:find_package("glu", opt) or package:find_package("libglu", opt)
             end
         end
+    end)
+
+    on_install("linux", function (package)
+        local configs = {}
+        table.insert(configs, "-Ddefault_library=" .. (package:config("shared") and "shared" or "static"))
+        import("package.tools.meson").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cincludes("GL/glu.h"))
     end)
